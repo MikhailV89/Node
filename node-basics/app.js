3e51f54851b4c5d69aea994d09e4090c1f9afa83
https://github.com/MikhailV89 <mvasilchenko89@gmail.com>
2025-02-03 17:42:28 +0200

    hw3 is ready

--- node-basics/app.js
+++ node-basics/app.js
@@ -1,9 +1,35 @@
-const fs = require('fs');
+const express = require("express");
+const app = express();
+const port = 3000;
 
-fs.readFile('data.txt', 'utf8', (err, data) => {
-    if (err) {
-        console.error("Помилка читання файлу:", err);
-        return;
+app.use((req, res, next) => {
+    console.log(`Запит: ${req.method} ${req.url}`);
+    next();
+});
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Привіт з Express.js!");
+});
+
+app.get("/user/:id", (req, res) => {
+    const userId = req.params.id;
+    res.send(`Користувач з ID: ${userId}`);
+});
+
+app.post("/submit", (req, res) => {
+    const { name, email } = req.body;
+    if (!name || !email) {
+        return res.status(400).send("Помилка: Вкажіть ім'я та електронну пошту!");
     }
-    console.log("Вміст файлу data.txt:", data);
+    res.send(`Отримано дані: Ім'я - ${name}, Електронна пошта - ${email}`);
+});
+
+app.use((req, res) => {
+    res.status(404).send("Сторінка не знайдена");
+});
+
+app.listen(port, () => {
+    console.log(`Сервер працює на http://localhost:${port}`);
 });
